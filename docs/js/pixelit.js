c2dee323b68ca8805ffd803299455b14dbe4b7f1
Dennis Hinzpeter <o0shojo0o@dietru.de>
2021-05-20 18:46:37 +0200

    Update pixelit.js

--- docs/js/pixelit.js
+++ docs/js/pixelit.js
@@ -95,7 +95,7 @@ function RefershData(input) {
             if (pageName == 'dash') {
                 switch (key) {
                     case "pixelitVersion":                                                
-                            jsonObj[key] = checkUpdateavailable(jsonObj[key]) ? `<i class="update" onclick="showChangelog();">${jsonObj[key]} update available!</i>  <a href="#" onclick="showChangelog();"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></a>` : jsonObj[key];                                       
+                            jsonObj[key] = checkUpdateavailable(jsonObj[key]) ? `<a href="#" class="update" onclick="showChangelog();">${jsonObj[key]} update available!</a>  <a href="#" onclick="showChangelog();"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></a>` : `<a href="#" onclick="showChangelog();">${jsonObj[key]}</a>`;                                       
                         break;
                     case "note":
                         if (!jsonObj[key].trim()) {
