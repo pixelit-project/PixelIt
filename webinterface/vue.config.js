bc02621b52e967f4d53507eea9245f8eb197310d
Dennis Hinzpeter <o0shojo0o@dietru.de>
2021-10-16 20:55:25 +0200

    clean

--- webinterface/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://o0shojo0o.github.io/PixelIt/new_webif/'
-    : '/'
-}
