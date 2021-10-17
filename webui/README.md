71eb244b6832593ca5bbba6451375827cacebda8
Dennis Hinzpeter <o0shojo0o@dietru.de>
2021-10-17 15:40:08 +0200

    add webui source

---
+++ webui/README.md
@@ -0,0 +1,19 @@
+![](https://raw.githubusercontent.com/o0shojo0o/ioBroker.pixelit/master/admin/pixelit.png)
+
+# PixelIt.WebInterface
+
+Vue base web interface for the [PixelIt projekt](https://github.com/o0shojo0o/PixelIt)
+
+## Changelog
+
+<!--
+### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+
+-   (o0shojo0o) add option for auto fallback animation
+
+### 0.0.1 (2021-10-16)
+
+-   (o0shojo0o) init
---
+++ webui/src/components/ListInfo.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-simple-table>
+        <template>
+            <tbody>
+                <tr v-for="item in items" :key="item.title">
+                    <td class="name">{{ item.name }}:</td>
+                    <td>
+                        {{ item.value }}
+                    </td>
+                </tr>
+            </tbody>
+        </template>
+    </v-simple-table>
+</template>
+
+<script>
+export default {
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style>
+.name {
+    font-weight: 600;
+    font-size: 16px !important;
+}
+</style>
