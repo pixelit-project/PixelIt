f2750a711c83f742cb34c52bc0f0b3711a67bfb4
Dennis Rathjen <dennis.rathjen@outlook.de>
2022-08-30 18:45:57 +0200

    update WebUI: new api calls

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -82,42 +82,7 @@ export default {
 
 async function getCurrentGitReleaseData(state) {
     try {
-        const gitData = await (await fetch("https://api.github.com/repos/pixelit-project/PixelIt/releases")).json();
-        state.gitReleases = [];
-        for (let i = 0; i < 4; i++) {
-            const data = {
-                version: gitData[i].name,
-                date: gitData[i].published_at.split("T")[0],
-                downloads: gitData[i].assets.reduce((result, x) => result + x.download_count, 0),
-                downloadURL: gitData[i].html_url,
-                fwdownloads: [],
-                releaseNoteArray: gitData[i].body.replaceAll("-", "").split("\r\n"),
-                readmeLink: `https://github.com/pixelit-project/PixelIt#${gitData[i].name.replaceAll(".", "")}-${gitData[i].published_at.split("T")[0]}`,
-            };
-
-            for (const asset of gitData[i].assets) {
-                let fwdownload;
-                // New filename format https://github.com/pixelit-project/PixelIt/pull/153
-                // firmware_v3.3.3_wemos_d1_mini32.bin
-                if (asset.name.includes("firmware_v")) {
-                    fwdownload = {
-                        name: asset.name.substring(asset.name.indexOf(asset.name.split("_")[2])),
-                        downloads: asset.download_count,
-                    };
-                    // Old filename format
-                    // firmware_wemos_d1_mini32.bin
-                } else {
-                    fwdownload = {
-                        name: asset.name.substring(asset.name.indexOf(asset.name.split("_")[1])),
-                        downloads: asset.download_count,
-                    };
-                }
-                fwdownload.name = fwdownload.name.replace(".bin", "").replaceAll("_", " ").toUpperCase();
-                data.fwdownloads.push(fwdownload);
-            }
-
-            state.gitReleases.push(data);
-        }
+        state.gitReleases = await (await fetch("https://pixelit.bastelbunker.de/api/releases")).json();
         state.gitVersion = state.gitReleases[0].version;
         state.gitDownloadUrl = state.gitReleases[0].downloadURL;
     } catch (error) {
@@ -126,7 +91,11 @@ async function getCurrentGitReleaseData(state) {
 }
 
 async function getUserMapData(state) {
-    state.userMapData = await (await fetch("https://pixelit.bastelbunker.de/API/GetUserMapData")).json();
+    try {
+        state.userMapData = await (await fetch("https://pixelit.bastelbunker.de/api/usermapdata")).json();
+    } catch (error) {
+        console.log(`getUserMapData: error (${error})`);
+    }
 }
 </script>
 
