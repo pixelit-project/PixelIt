23546546ff394d8d78847c503a81b67d4cd55b25
Fab!an <foorschtbar@users.noreply.github.com>
2022-10-12 19:36:08 +0200

    Moved WebUI to different repo and build WebUI in pipeline (#220)
    * WIP
    
    * Restored Webnterface.h
    
    * Moved WebUI to extra repo
    
    * Deploy WebUI only with Tag
    
    * Removed docs folder

---
+++ .github/webui.py
@@ -0,0 +1,17 @@
+# Read content
+html = open('./pixelit-webui-artifact/index.html', 'r')
+o = html.read()
+content = o.replace('\n', '')
+html.close()
+
+# Print the content of the file
+print(content)
+
+# HTML content to C++ string
+content = "const char mainPage[] PROGMEM = R\"=====(" + content + ")=====\";"
+
+# Write the content of the file
+webuih = open('./src/Webinterface.h', 'w')
+webuih.write(content)
+webuih.close()
+
--- webui/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://pixelit-project.github.io/PixelIt/webui/' : '/'
-}
