b7f69255461eef9d91d16df1e900a90690b79027
foorschtbar <mail@wischmopp90.com>
2023-02-12 02:09:48 +0100

    Setup VERSION to a placeholder (will be replaced by build script)

--- src/PixelIt.ino
+++ src/PixelIt.ino
@@ -52,7 +52,7 @@
 #define CHECKUPDATESCREEN_INTERVAL 1000 * 60 * 5 // 5 Minutes
 #define CHECKUPDATESCREEN_DURATION 1000 * 5      // 5 Seconds
 
-#define VERSION "2.3.0-beta"
+#define VERSION "0.0.0-beta" // will be replaced by build script with tag!
 
 void FadeOut(int = 10, int = 0);
 void FadeIn(int = 10, int = 0);
